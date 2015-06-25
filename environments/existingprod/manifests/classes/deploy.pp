b277810a27370485d99e361a831b1bf96de7ac58
udit jain <udit.jain@flipkart.com>
2015-06-25 17:02:00 +0530

    change in deploy file

--- environments/existingprod/manifests/classes/deploy.pp
+++ environments/existingprod/manifests/classes/deploy.pp
@@ -20,7 +20,19 @@ class deploy {
     file { "/home/udit.jain/health-check.sh":
             owner => root,
             group => root,
-            content => "APT::Get::AllowUnauthenticated yes;",
+            content => "hostname=`hostname`
+                        status=`curl -I "http://$hostname.nm.flipkart.com:25280/sherlock/intent?q=samsung" | grep "HTTP" | awk '{print $2}'`
+
+                        if [ "$status" == "200" ]
+                        then
+                            echo "========curl command successfull========"
+                            echo "status $status"
+                            exit 0
+                        else
+                            echo "===========curl command failed=========="
+                            echo "status = $status"
+                            exit 2
+                        fi",
             mode => 777,
     }
 
