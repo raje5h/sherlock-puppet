9ab58ee2e67c6e3933817d44d8dbdd31c9f3a430
udit jain <udit.jain@flipkart.com>
2015-06-25 19:52:16 +0530

    modified health script tempate

--- templates/common/health-script.sh
+++ templates/common/health-script.sh
@@ -1,7 +1,7 @@
 hostname=`hostname`
 status=`curl -I "http://$hostname.nm.flipkart.com:25280/sherlock/intent?q=samsung" | grep "HTTP" | awk '{print $2}'`
 
-if [ "$status" == "200" ]
+if [ $status -eq 200 ]
 then
     echo "========curl command successfull========"
     echo "status $status"
