5d2cb456e7fa94c5039d5053b2d6fe405cfa5dfe
udit jain <udit.jain@flipkart.com>
2015-06-25 18:45:40 +0530

    manifest file for newdcprod env

--- environments/newdcprod/manifests/classes/deploy.pp
+++ environments/newdcprod/manifests/classes/deploy.pp
@@ -1,16 +1,16 @@
 class deploy {
 
-    $Version = "1.1434364148-01nm"
-    $env = "9"
+    $envVersion = "HEAD"
+    $envName = "sherlock-sample-env"
 
-    file {"/etc/apt/apt.conf.d/99auth":
+    file { "/etc/apt/apt.conf.d/99auth":
         owner => root,
         group => root,
         content => "APT::Get::AllowUnauthenticated yes;",
         mode => 644,
     }
 
-    file {"/etc/apt/sources.list.d/source1.list":
+    file { "/etc/apt/sources.list.d/source1.list":
         owner => root,
         group => root,
         content => "deb http://d42-a-0002.nm.flipkart.com:8080/repos/infra-cli/3 /",
@@ -18,39 +18,39 @@ class deploy {
         require => File["/etc/apt/apt.conf.d/99auth"],
     }
     
-    exec {"apt-update-1":
+    exec { "apt-update-1":
         command => "sudo apt-get update",
         path => "/usr/bin/",
-	require => File["/etc/apt/sources.list.d/source1.list"],
+	    require => File["/etc/apt/sources.list.d/source1.list"],
     }
 
     package { "infra-cli":
-	ensure => installed,
+	    ensure => installed,
         require => Exec[apt-update-1],
     }
 
     exec { "infra-cli-command":
-    	command => "reposervice --host repo-svc-app-0001.nm.flipkart.com --port 8080 getenv --name sherlock-sample-env --appkey $env --version HEAD > /etc/apt/sources.list.d/source1.list",
-	path => "/usr/bin/",
-	require => Package[infra-cli],
+    	command => "reposervice --host repo-svc-app-0001.nm.flipkart.com --port 8080 getenv --name $envName --appkey 12 --version $envVersion > /etc/apt/sources.list.d/source1.list",
+	    path => "/usr/bin/",
+	    require => Package[infra-cli],
     }
     
-    exec {"apt-update-2":
+    exec { "apt-update-2":
         command => "sudo apt-get update",
         path => "/usr/bin/",
-	require => Exec[infra-cli-command],
+	    require => Exec[infra-cli-command],
     }
     
     exec { "fk-w3-sherlock":
         command => "sudo apt-get -y --force-yes install fk-w3-sherlock=1.1434017924-01nm",
         path => "/usr/bin",
         logoutput => true,
-	tries => 3,
+	    tries => 2,
         timeout => 50,
         require => Exec["apt-update-2"],
     }
 
-    exec {"health-check-script":
+    exec { "health-check-script":
          command => "sh /home/udit.jain/health-script.sh",
          path => [ "/bin/", "/usr/bin/" ],
          logoutput => true,
