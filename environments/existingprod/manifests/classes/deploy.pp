62b44dddce049c386765bd3f4f7d2246e9a11eb1
udit jain <udit.jain@flipkart.com>
2015-06-25 17:09:21 +0530

    Adding templates

--- environments/existingprod/manifests/classes/deploy.pp
+++ environments/existingprod/manifests/classes/deploy.pp
@@ -20,19 +20,7 @@ class deploy {
     file { "/home/udit.jain/health-check.sh":
             owner => root,
             group => root,
-            content => "hostname=`hostname`
-                        status=`curl -I "http://$hostname.nm.flipkart.com:25280/sherlock/intent?q=samsung" | grep "HTTP" | awk '{print $2}'`
-
-                        if [ "$status" == "200" ]
-                        then
-                            echo "========curl command successfull========"
-                            echo "status $status"
-                            exit 0
-                        else
-                            echo "===========curl command failed=========="
-                            echo "status = $status"
-                            exit 2
-                        fi",
+            content => template("common/health-script.sh")
             mode => 777,
     }
 
