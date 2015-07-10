4b846827504919bb6d8f4c1a49e774c7315ff8d5
udit jain <udit.jain@flipkart.com>
2015-07-10 11:59:54 +0530

    Adding custom facts

--- environments/existingprod/manifests/classes/deploy.pp
+++ environments/existingprod/manifests/classes/deploy.pp
@@ -1,6 +1,8 @@
 class deploy {
 
     $packageVersion = hiera('version')
+    $newversion = $::sherlockversion
+    $currentRotationStatus = $::rotationstatus
 
     exec { "apt-update":
         command => "sudo apt-get update",
@@ -12,26 +14,31 @@ class deploy {
         command => "sudo apt-get -y --force-yes install fk-w3-sherlock=$packageVersion",
         path => "/usr/bin",
         logoutput => true,
-	    tries => 2,
+        tries => 2,
         timeout => 600,
         require => Exec["apt-update"],
+    }
+
+    exec { "check-fact":
+        command => "echo $newversion",
+        path => ["/bin/", "/usr/bin" ],
+        require => Exec["fk-w3-sherlock"],
     }
 
     file { "/home/udit.jain/health-script.sh":
         owner => root,
         group => root,
-        content => template("common/health-script.sh"),
+        content => template("common/health-script"),
         mode => 777,
         require => Exec["fk-w3-sherlock"],
     }
 
     exec { "health-check-script":
-         command => "sh /home/udit.jain/health-script.sh",
-         path => [ "/bin/", "/usr/bin/" ],
-         logoutput => true,
-         timeout => 900,
-         require => File["/home/udit.jain/health-script.sh"],
+        command => "sh /home/udit.jain/health-script.sh",
+        path => [ "/bin/", "/usr/bin/" ],
+        logoutput => true,
+        timeout => 900,
+        require => File["/home/udit.jain/health-script.sh"],
     }
 }
-
 
---
+++ modules/sherlock-package/lib/facter/rotationstatus.rb
@@ -0,0 +1,3 @@
+facter.add(:rotationstatus) do
+  setcode 'fk-w3-sherlock-admin status'
+end
