fd9207544f20abee94de56f3c4b918129b64f33e
Vishal Goel <vishal.goel@flipkart.com>
2015-08-04 18:26:31 +0530

    testing restartprod

--- environments/restartprod/manifests/classes/deploy.pp
+++ environments/restartprod/manifests/classes/deploy.pp
@@ -3,9 +3,32 @@ class deploy {
     $packageVersion = hiera('version')
     $currentRotationStatus = $::rotationstatus
 
-    exec { "admin-status":
-       command => "sudo fk-w3-sherlock-admin status",
-       logoutput => true,
-       path => "/usr/bin/",
+    if $currentRotationStatus == "In rotation"
+    {
+        exec { "admin-status":
+            command => "sudo fk-w3-sherlock-admin status",
+            logoutput => true,
+            path => "/usr/bin/",
+        }
+
+        /*exec { "stop":
+            command => "sudo /etc/init.d/fk-w3-sherlock stop",
+            logoutput => true,
+            path => "/usr/bin/",
+        }
+
+        exec { "start":
+            command => "sudo /etc/init.d/fk-w3-sherlock start",
+            logoutput => true,
+            path => "/usr/bin/",
+            require => Exec["stop"],
+        }
+
+        exec { "admin-bir":
+            command => "sudo fk-w3-sherlock-admin bir",
+            logoutput => true,
+            path => "/usr/bin/",
+            require => Exec["start"],
+        }*/
     }
 }
