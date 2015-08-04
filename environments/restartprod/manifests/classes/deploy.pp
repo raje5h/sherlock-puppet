490a997d7324f0516101fbe1fb31fc2ef21635c0
Vishal Goel <vishal.goel@flipkart.com>
2015-08-04 18:22:44 +0530

    restartprod edited

--- environments/restartprod/manifests/classes/deploy.pp
+++ environments/restartprod/manifests/classes/deploy.pp
@@ -8,29 +8,4 @@ class deploy {
        logoutput => true,
        path => "/usr/bin/",
     }
-
-    //$output = generate("/usr/bin/sudo fk-w3-sherlock-admin status");
-
-    /*if $currentRotationStatus == "In rotation" {
-
-        exec { "stop":
-            command => "sudo /etc/init.d/fk-w3-sherlock stop",
-            logoutput => true,
-            path => "/usr/bin/",
-        }
-
-        exec { "start":
-            command => "sudo /etc/init.d/fk-w3-sherlock start",
-            logoutput => true,
-            path => "/usr/bin/",
-            require => Exec["stop"],
-        }
-
-        exec { "admin-bir":
-            command => "sudo fk-w3-sherlock-admin bir",
-            logoutput => true,
-            path => "/usr/bin/",
-            require => Exec["start"],
-        }
-    }*/
 }
