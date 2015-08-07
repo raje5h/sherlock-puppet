f5caad5621585d2a9000ea750065a390c271c7ee
Vishal Goel <vishal.goel@flipkart.com>
2015-08-07 16:38:23 +0530

    testing onlyif

--- environments/restartprod/manifests/classes/deploy.pp
+++ environments/restartprod/manifests/classes/deploy.pp
@@ -2,26 +2,27 @@ class deploy {
 
     $currentRotationStatus = $::rotationstatus
 
-    if $currentRotationStatus == "In rotation" {
-
-        exec { "stop":
+    exec { "stop":
             command => "sudo /etc/init.d/fk-w3-sherlock stop",
             logoutput => true,
             path => "/usr/bin/",
         }
 
-        exec { "start":
+    exec { "start":
             command => "sudo /etc/init.d/fk-w3-sherlock start",
             logoutput => true,
             path => "/usr/bin/",
             require => Exec["stop"],
         }
 
+    //if $currentRotationStatus == "In rotation" {
+
         exec { "admin-bir":
             command => "sudo fk-w3-sherlock-admin bir",
             logoutput => true,
             path => "/usr/bin/",
+            onlyif => [$currentRotationStatus == "In rotation"]
             require => Exec["start"],
         }
-    }
+    //}
 }
