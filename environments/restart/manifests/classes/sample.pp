63254afb4d8165b163644b4cd43d6aff5a365b92
Vishal Goel <vishal.goel@flipkart.com>
2015-09-15 16:03:35 +0530

    minor change

--- environments/restart/manifests/classes/sample.pp
+++ environments/restart/manifests/classes/sample.pp
@@ -9,4 +9,18 @@ class sample {
         command => "sudo apt-get update",
         path => "/usr/bin/",
     }
+
+    exec { "restart":
+            command => "sudo /etc/init.d/fk-w3-hudson restart",
+            logoutput => true,
+            path => "/usr/bin/",
+        }
+
+    exec { "bir":
+            command => "sudo fk-w3-hudson-admin bir",
+            logoutput => true,
+            path => "/usr/bin/",
+            tries => 2,
+            require => Exec["restart"],
+        }
 }
