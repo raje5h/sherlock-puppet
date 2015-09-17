f803003ab8f0cf71e5d8f7fe8f7e2d7010e773b0
Vishal Goel <vishal.goel@flipkart.com>
2015-09-17 13:53:14 +0530

    testing env issue

--- environments/newdcrestart/manifests/classes/sample.pp
+++
@@ -1,7 +0,0 @@
-class sample {
-    
-    exec { "apt-update":
-        command => "sudo apt-get update",
-        path => "/usr/bin/",
-    }
-}
--- environments/newdcrestart/manifests/nodes/hudson-app.pp
+++
@@ -1,3 +0,0 @@
-node /^hudson-app/ {
-        include sample
-}
