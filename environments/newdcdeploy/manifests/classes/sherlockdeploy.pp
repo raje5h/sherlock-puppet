da902bb0ed69244f95375ca77911e3f8dc489243
Vishal Goel <vishal.goel@flipkart.com>
2015-09-24 18:50:25 +0530

    sherlockdeploy edited in newdc

--- environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
+++ environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
@@ -1,13 +1,13 @@
 class sherlockdeploy {
 
     $envVersion = "HEAD"
-    $envName = "sherlock-sample-env"
+    $envName = "sherlock-app-env"
     $host = "repo-svc-app-0001.nm.flipkart.com"
-    $port = "8080"
+    $repo_svc_port = "8080"
     $appkey = "12"
 
     exec { "infra-cli-command":
-    	command => "reposervice --host $host --port $port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
+    	command => "reposervice --host $host --port $repo_svc_port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
 	    path => "/usr/bin/",
     }
     
@@ -18,7 +18,7 @@ class sherlockdeploy {
     }
     
     exec { "fk-w3-sherlock":
-        command => "sudo apt-get -y --force-yes install fk-w3-sherlock",
+        command => "sudo apt-get -y --allow-unauthenticated --force-yes install fk-w3-sherlock",
         path => "/usr/bin",
         logoutput => true,
 	    tries => 2,
@@ -26,7 +26,7 @@ class sherlockdeploy {
         require => Exec["apt-get-update"],
     }
 
-    file { "/home/vishal.goel/health-script.sh":
+    file { "/etc/default/health-script.sh":
         owner => root,
         group => root,
         content => template("common/health-script"),
@@ -34,12 +34,12 @@ class sherlockdeploy {
         require => Exec["fk-w3-sherlock"],
     }
 
-    #exec { "health-check-script":
-    #     command => "sh /home/vishal.goel/health-script.sh",
-    #     path => [ "/bin/", "/usr/bin/" ],
-    #     logoutput => true,
-    #     require => Exec["/home/vishal.goel/health-script.sh"]
-    #}
+    exec { "health-check-script":
+         command => "sh /etc/default/health-script.sh",
+         path => [ "/bin/", "/usr/bin/" ],
+         logoutput => true,
+         require => Exec["/home/vishal.goel/health-script.sh"]
+    }
 }
 
 
