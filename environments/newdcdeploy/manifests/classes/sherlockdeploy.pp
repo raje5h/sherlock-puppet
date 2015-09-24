8ade5b313feb70e075093639baa7746ac98539c7
Vishal Goel <vishal.goel@flipkart.com>
2015-09-24 18:09:20 +0530

    changes in sherlockdeploy in newdc env

--- environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
+++ environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
@@ -2,61 +2,44 @@ class sherlockdeploy {
 
     $envVersion = "HEAD"
     $envName = "sherlock-sample-env"
-
-    file { "/etc/apt/apt.conf.d/99auth":
-        owner => root,
-        group => root,
-        content => "APT::Get::AllowUnauthenticated yes;",
-        mode => 644,
-    }
-
-    file { "/etc/apt/sources.list.d/source1.list":
-        owner => root,
-        group => root,
-        content => "deb http://d42-a-0002.nm.flipkart.com:8080/repos/infra-cli/3 /",
-        mode => 644,
-        require => File["/etc/apt/apt.conf.d/99auth"],
-    }
-    
-    exec { "apt-update-1":
-        command => "sudo apt-get update",
-        path => "/usr/bin/",
-	    require => File["/etc/apt/sources.list.d/source1.list"],
-    }
-
-    package { "infra-cli":
-	    ensure => installed,
-        require => Exec[apt-update-1],
-    }
+    $host = "repo-svc-app-0001.nm.flipkart.com"
+    $port = "8080"
+    $appkey = "12"
 
     exec { "infra-cli-command":
-    	command => "reposervice --host repo-svc-app-0001.nm.flipkart.com --port 8080 getenv --name $envName --appkey 12 --version $envVersion > /etc/apt/sources.list.d/source1.list",
-	    path => "/u
-        sr/bin/",
-	    require => Package[infra-cli],
+    	command => "reposervice --host $host --port $port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
+	    path => "/usr/bin/",
     }
     
-    exec { "apt-update-2":
+    exec { "apt-get-update":
         command => "sudo apt-get update",
         path => "/usr/bin/",
-	    require => Exec[infra-cli-command],
+	    require => Exec["infra-cli-command"],
     }
     
     exec { "fk-w3-sherlock":
-        command => "sudo apt-get -y --force-yes install fk-w3-sherlock=1.1434017924-01nm",
+        command => "sudo apt-get -y --force-yes install fk-w3-sherlock",
         path => "/usr/bin",
         logoutput => true,
 	    tries => 2,
         timeout => 600,
-        require => Exec["apt-update-2"],
+        require => Exec["apt-get-update"],
     }
 
-    exec { "health-check-script":
-         command => "sh /home/udit.jain/health-script.sh",
-         path => [ "/bin/", "/usr/bin/" ],
-         logoutput => true,
-         require => Exec["fk-w3-sherlock"]
+    file { "/home/vishal.goel/health-script.sh":
+        owner => root,
+        group => root,
+        content => template("common/health-script"),
+        mode => 777,
+        require => Exec["fk-w3-sherlock"],
     }
+
+    #exec { "health-check-script":
+    #     command => "sh /home/vishal.goel/health-script.sh",
+    #     path => [ "/bin/", "/usr/bin/" ],
+    #     logoutput => true,
+    #     require => Exec["/home/vishal.goel/health-script.sh"]
+    #}
 }
 
 
