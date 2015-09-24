0911775355dcb459ba6124adc8fbb99907d6f04d
Vishal Goel <vishal.goel@flipkart.com>
2015-09-24 20:34:50 +0530

    minor changes

--- environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
+++ environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
@@ -5,12 +5,12 @@ class sherlockdeploy {
 
     $envVersion = "HEAD"
     $envName = "sherlock-app-env"
-    $host = "repo-svc-app-0001.nm.flipkart.com"
+    $repo_svc_host = "repo-svc-app-0001.nm.flipkart.com"
     $repo_svc_port = "8080"
     $appkey = "12"
 
     exec { "infra-cli-command":
-    	command => "reposervice --host $host --port $repo_svc_port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
+    	command => "reposervice --host $repo_svc_host --port $repo_svc_port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
 	    path => "/usr/bin/",
     }
     
