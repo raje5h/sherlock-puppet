30b7daee4af9344b8766785a009b8e07ee016605
udit jain <udit.jain@flipkart.com>
2015-10-06 17:44:20 +0530

    adding cosmos

--- environments/deploy/manifests/classes/sherlocksetup.pp
+++ environments/deploy/manifests/classes/sherlocksetup.pp
@@ -2,8 +2,13 @@ class sherlocksetup {
 
     $envVersion = "12"
     $envName = "sherlock-app-env"
+    
     $alertzEnvName = "sherlock-alertz-env"
     $alertzEnvVersion = "1"
+    
+    $cosmosEnvVersion = "1"
+    $cosmosEnvName = "sherlock-cosmos-env"
+
     $repo_svc_host = "repo-svc-app-0001.nm.flipkart.com"
     $repo_svc_port = "8080"
     $appkey = "12"
@@ -125,6 +130,80 @@ class sherlocksetup {
         path => "/usr/bin/",
         logoutput => true,
         require => Exec["kill dpkg"],
+    }
+    
+    
+    exec { "cosmos sources list":
+        command => "reposervice --host $repo_svc_host --port $repo_svc_port getenv --name $cosmosEnvName --appkey $appkey --version $cosmosEnvVersion > /etc/apt/sources.list.d/sherlock-cosmos.list",
+        path => "/usr/bin/",
+        require => Exec["fk-w3-sherlock"]
+    }
+
+    exec { "cosmos-service=sherlock-app":
+        command => "sudo echo 'sherlock-app' > /etc/default/cosmos-service",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos sources list"],
+    }
+
+    exec { "sudo apt-get update cosmos":
+        command => "sudo apt-get update ",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-service=sherlock-app"],
+    }
+
+    exec { "fk-ops-sgp-sherlock install":
+        command => "sudo apt-get install --yes --allow-unauthenticated fk-ops-sgp-sherlock --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["sudo apt-get update cosmos"],
+    }
+
+    exec { "fk-config-service-confd":
+        command => "sudo apt-get install --yes --allow-unauthenticated fk-config-service-confd  --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["fk-ops-sgp-sherlock install"],
+    }
+
+    exec { "stream-relay":
+        command => "sudo apt-get install --yes --allow-unauthenticated stream-relay --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["fk-config-service-confd"],
+    }
+
+    exec { "cosmos-collectd":
+        command => "sudo apt-get -o Dpkg::Options::='--force-confdef' install --yes --allow-unauthenticated cosmos-collectd  --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        logoutput => true,
+        require => Exec["stream-relay"],
+    }
+
+    exec { "cosmos-tail":
+        command => "sudo apt-get install --yes --allow-unauthenticated cosmos-tail --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-collectd"],
+    }
+
+    exec { "cosmos-jmx install":
+        command => "sudo apt-get install --yes --allow-unauthenticated cosmos-jmx --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-tail"],
+    }
+
+    exec { "cosmos-statsd":
+        command => "sudo apt-get install --yes --allow-unauthenticated cosmos-statsd  --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-jmx install"],
+    }
+
+    exec { "cosmos-jmx restart":
+        command => "sudo /etc/init.d/cosmos-jmx restart",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-statsd"],
+    }
+
+    exec { "fk-ops-sgp-sherlock reinstall":
+        command => "sudo apt-get install --yes --allow-unauthenticated fk-ops-sgp-sherlock --reinstall",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["cosmos-jmx restart"],
     }
 }
 
