b64b761a45e685cdd257eb10813b986d7365895f
Vishal Goel <vishal.goel@flipkart.com>
2015-10-14 15:32:23 +0530

    testing if condition

--- environments/restart/manifests/classes/sample.pp
+++ environments/restart/manifests/classes/sample.pp
@@ -1,6 +1,15 @@
 class sample {
+
+	$x = "a"
     
     exec { "apt-update-1":
+        command => "sudo apt-get update",
+        path => "/usr/bin/",
+    }
+
+    if $currentRotationStatus == "a" {
+
+        exec { "apt-update-2":
         command => "sudo apt-get update",
         path => "/usr/bin/",
     }
