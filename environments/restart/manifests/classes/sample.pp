894ebaac00baeb3a4eb797821c72c33ffc185d3e
Vishal Goel <vishal.goel@flipkart.com>
2015-10-14 16:43:00 +0530

    testing if condition

--- environments/restart/manifests/classes/sample.pp
+++ environments/restart/manifests/classes/sample.pp
@@ -1,14 +1,14 @@
 class sample {
 
 	$x = $::rotationstatus
-    
+    $flag = 0
     exec { "apt-update-1":
         command => "sudo apt-get update",
         path => "/usr/bin/",
     }
 
     if ($x == "In rotation") {
-
+    	flag = 1
         exec { "apt-update-2":
         command => "sudo apt-get update",
         path => "/usr/bin/",
@@ -16,11 +16,13 @@ class sample {
 
     }
 
-    file { "/etc/default/restart-health-script.sh":
+	if ($flag == 1) {
+    	file { "/etc/default/restart-health-script.sh":
             owner => root,
             group => root,
             content => template("common/hudsonrestart-health-script"),
             mode => 777,
             require => Exec["apt-update-2"],
-    }
+    	}
+	}
 }
