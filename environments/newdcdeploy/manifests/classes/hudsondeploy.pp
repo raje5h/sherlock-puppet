22be292f3e4c60008167c389e496510fdcb196c5
udit jain <udit.jain@flipkart.com>
2015-12-01 17:25:45 +0530

    hudson env version 13

--- environments/newdcdeploy/manifests/classes/hudsondeploy.pp
+++ environments/newdcdeploy/manifests/classes/hudsondeploy.pp
@@ -3,7 +3,7 @@ class hudsondeploy {
     $currentRotationStatus = $::hudsonrotationstatus
     $inRotation = "In Rotation"
 
-    $envVersion = "12"
+    $envVersion = "13"
     $envName = "sherlock-hudson-env"
     $repo_svc_host = "repo-svc-app-0001.nm.flipkart.com"
     $repo_svc_port = "8080"
