2fcf495a0880a9ac363692476dc9773188528734
udit jain <udit.jain@flipkart.com>
2015-12-24 17:27:27 +0530

    adding repo stpes back

--- environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
+++ environments/newdcdeploy/manifests/classes/sherlockdeploy.pp
@@ -9,9 +9,26 @@ class sherlockdeploy {
     $repo_svc_port = "8080"
     $appkey = "12"
 
+    exec { "infra-cli-source":
+        command => "sudo echo 'deb http://10.47.2.22:80/repos/infra-cli/3 /' > /etc/apt/sources.list.d/infra-cli-svc.list",
+        path => "/usr/bin/",
+    }
+
+    exec { "apt-get-update-infra":
+        command => "sudo apt-get update",
+        path => "/usr/bin/",
+        require => Exec["infra-cli-source"],
+    }
+    
+    exec { "infra-cli-install":
+        command => "sudo apt-get install --yes --allow-unauthenticated infra-cli",
+        path => "/usr/bin/",
+        require => Exec["apt-get-update-infra"],
+    }
     exec { "infra-cli-command":
-    	command => "reposervice --host $repo_svc_host --port $repo_svc_port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
-	    path => "/usr/bin/",
+    	  command => "reposervice --host $repo_svc_host --port $repo_svc_port getenv --name $envName --appkey $appkey --version $envVersion > /etc/apt/sources.list.d/sherlock.list",
+	      path => "/usr/bin/",
+	      require => Exec["infra-cli-install"],
     }
     
     exec { "apt-get-update":
