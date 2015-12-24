eaf03d5e529231b2c6619455cdd427ff7f34ae6a
udit jain <udit.jain@flipkart.com>
2015-12-24 18:54:45 +0530

    fix

--- environments/restart/manifests/classes/sherlockrestart.pp
+++ environments/restart/manifests/classes/sherlockrestart.pp
@@ -2,27 +2,17 @@ class sherlockrestart {
 
     $currentRotationStatus = $::rotationstatus
 
-    exec { "stop":
-            command => "sudo /etc/init.d/fk-w3-sherlock stop",
+    exec { "restart":
+            command => "sudo /etc/init.d/fk-w3-sherlock restart",
             logoutput => true,
             path => "/usr/bin/",
         }
-
-    exec { "start":
-            command => "sudo /etc/init.d/fk-w3-sherlock start",
-            logoutput => true,
-            path => "/usr/bin/",
-            require => Exec["stop"],
-        }
-
-    if $currentRotationStatus == "In rotation" {
 
         exec { "admin-bir":
             command => "sudo fk-w3-sherlock-admin bir",
             logoutput => true,
             path => "/usr/bin/",
-            #onlyif => [$currentRotationStatus == "In rotation"]
-            require => Exec["start"],
+            require => Exec["restart"],
         }
-    }
+    
 }
