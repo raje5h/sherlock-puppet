2bce21ff021fb4fa9e059e6bff521ef6bb2e37be
udit jain <udit.jain@flipkart.com>
2016-01-06 16:52:40 +0530

    dfsd

--- environments/sherlockIaasSetup/manifests/classes/sherlockshard1.pp
+++ environments/sherlockIaasSetup/manifests/classes/sherlockshard1.pp
@@ -2,7 +2,7 @@ class sherlockshard1 {
 
    exec { "sudo-apt-get-update-1":
         command => "sudo apt-get update",
-        path => "/usr/bin/",
-        require => Exec[sudo-apt-get-update-2]
+        logoutput => true,
+        path => "/usr/bin/"
     }
 }
