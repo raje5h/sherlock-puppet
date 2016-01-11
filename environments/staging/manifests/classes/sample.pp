9e2d3e5115c0e715f31bfae3a0bb27b3016cc6cd
udit jain <udit.jain@flipkart.com>
2016-01-11 12:28:36 +0530

    echo fix

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -2,7 +2,8 @@ class sample {
     $foo = $::foo
     
     exec {"ap-get-update":
-        command => "echo $foo",
+        command => "echo $foo 234",
+        logoutput => true,
         path => ["/bin/", "/usr/bin/"],
     }
 }
