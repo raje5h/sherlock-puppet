cd8a2c01b01bd8b4dcaecb4d91ce8b6f59728a68
udit jain <udit.jain@flipkart.com>
2016-01-12 17:45:51 +0530

    testing

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -1,16 +1,10 @@
 class sample {
-    $foovar = $::bar
+     
     $bucket = $::configbucket
     
-    exec { "test-facter":
-        command => "echo $bucket",
-        logoutput => true,  
-        path => ["/bin/", "/usr/bin/"],
-    }
-    
-    exec {"ap-get-update":
-        command => "echo $foovar",
-        logoutput => true,
-        path => ["/bin/", "/usr/bin/"],
+    exec { "update-cluster-name":
+        command => "sudo echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq .'keys.\"conman-cluster-name\"'`
+        > /etc/default/cluster-name", 
+        path => "/usr/bin/",
     }
 }
