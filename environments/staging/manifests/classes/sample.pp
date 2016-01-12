7427058a415e734a0c12d5603844f738b00e65ec
udit jain <udit.jain@flipkart.com>
2016-01-12 18:01:09 +0530

    testing

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -3,7 +3,7 @@ class sample {
     $bucket = $::configbucket
     
     exec { "update-cluster-name":
-        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq .'keys.\"conman-cluster-name\"'` | sudo tee --append /etc/default/cluster-name", 
+        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq .'keys.\"conman-cluster-name\"' | tr -d '\"'` | sudo tee --append /etc/default/cluster-name", 
         path => [ "/bin/", "/usr/bin" ],
     }
 }
