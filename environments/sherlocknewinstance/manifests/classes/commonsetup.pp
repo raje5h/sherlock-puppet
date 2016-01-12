fbd17e5fa44df786756f961c5a9bae6c58e8e025
udit jain <udit.jain@flipkart.com>
2016-01-12 16:10:53 +0530

    adding etc host entry

--- environments/sherlocknewinstance/manifests/classes/commonsetup.pp
+++ environments/sherlocknewinstance/manifests/classes/commonsetup.pp
@@ -46,10 +46,16 @@ class commonsetup {
         require => Exec["adding-host-4"],
     }
     
+    exec { "adding-host-6":
+        command => "echo '10.84.35.1    pf-config-publish.nm.flipkart.com' | sudo tee --append /etc/hosts",
+        path => [ "/bin/", "/usr/bin" ] ,
+        require => Exec["adding-host-5"],
+    }
+    
     exec { "disk-mount":
       command => "/sbin/mkfs.ext4 /dev/vdb && echo \"/dev/vdb /var/lib/fk-w3-sherlock ext4 rw,noatime,nodiratime 0 0\" >>/etc/fstab && (cd /var/lib/;mkdir fk-w3-sherlock) && mount /dev/vdb /var/lib/fk-w3-sherlock",
       path => [ "/bin/", "/usr/bin" ],
-      require => Exec["adding-host-5"],
+      require => Exec["adding-host-6"],
       
     }
 }
