205a0e39e472dc60fa6f03ba2951a1e5230afe68
udit jain <udit.jain@flipkart.com>
2016-01-12 18:02:59 +0530

    testing

--- environments/sherlocknewinstance/manifests/classes/application.pp
+++ environments/sherlocknewinstance/manifests/classes/application.pp
@@ -12,9 +12,8 @@ class application {
     $bucket = $::configbucket
     
     exec { "update-cluster-name":
-        command => "sudo echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq .'keys.\"conman-cluster-name\"'`
-        > /etc/default/cluster-name", 
-        path => "/usr/bin/",
+        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq .'keys.\"conman-cluster-name\"' | tr -d '\"'` | sudo tee --append /etc/default/cluster-name", 
+        path => [ "/bin/", "/usr/bin" ],
         require => Exec["disk-mount"],
     }
     
