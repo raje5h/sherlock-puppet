1a6ee39109d6052624773a3f67bb0e1da2b6d720
udit jain <udit.jain@flipkart.com>
2016-01-13 16:18:12 +0530

    sdfkskd

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -2,10 +2,10 @@ class sample {
      
     $bucket = $::configbucket
     $cluster_fact = $::uniquefact
-    $conman_cluster_name = "{conman-cluster-name}${cluster_fact}"
+    $conman_cluster_name = "conman-cluster-name"
     
     exec { "update-cluster-name":
-        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq  --arg cluster $conman_cluster_name  .'keys[$cluster]'` | sudo tee --append /etc/default/cluster-name", 
+        command => "conman_cluser_name=\"conman-cluster-name.\"$cluster_fact && echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq  --arg cluster $conman_cluster_name  .'keys[$cluster]'` | sudo tee --append /etc/default/cluster-name", 
         path => [ "/bin/", "/usr/bin" ],
     }
 }
