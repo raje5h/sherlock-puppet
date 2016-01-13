4c6c7d2af99b9f295cc0f4292b4c555be760b865
udit jain <udit.jain@flipkart.com>
2016-01-13 17:20:22 +0530

    sdfkskd

--- environments/sherlocknewinstance/manifests/classes/application.pp
+++ environments/sherlocknewinstance/manifests/classes/application.pp
@@ -11,12 +11,11 @@ class application {
     
     $bucket = $::configbucket
     $cluster_fact = $::uniquefact
-    $conman_cluster_name = "{conman-cluster-name}${cluster_fact}"
+    $conman_cluster_name = "conman-cluster-name.${cluster_fact}"
     
     exec { "update-cluster-name":
-        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq  --arg cluster $conman_cluster_name  .'keys[$cluster]'` | sudo tee --append /etc/default/cluster-name", 
         path => [ "/bin/", "/usr/bin" ],
-        require => Exec["disk-mount"],
+        command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | grep -o '\"$conman_cluster_name\":[^,]*' | cut -d '\"' -f4` | sudo tee --append /etc/default/cluster-name", 
     }
     
     exec { "infra-cli-command":
