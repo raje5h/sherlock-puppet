62294c189b4f62d4c1c1418964c40656482b8401
udit jain <udit.jain@flipkart.com>
2016-01-13 17:05:14 +0530

    sdfkskd

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -2,7 +2,7 @@ class sample {
      
     $bucket = $::configbucket
     $cluster_fact = $::uniquefact
-    $conman_cluster_name = "conman-cluster-name${cluster_fact}"
+    $conman_cluster_name = "conman-cluster-name$cluster_fact"
     
     exec { "update-cluster-name":
         path => [ "/bin/", "/usr/bin" ],
