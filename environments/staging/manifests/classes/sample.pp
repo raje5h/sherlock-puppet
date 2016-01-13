82602d68a783fb87e26fbd3fd4c4c63245789063
udit jain <udit.jain@flipkart.com>
2016-01-13 16:23:29 +0530

    sdfkskd

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -5,7 +5,7 @@ class sample {
     $conman_cluster_name = "conman-cluster-name"
     
     exec { "update-cluster-name":
-        command => "export conman_cluser_name=\"conman-cluster-name.\"$cluster_fact && echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq  --arg cluster $conman_cluster_name  .'keys[$cluster]'` | sudo tee --append /etc/default/cluster-name", 
+        command => "conman_cluser_name=\"conman-cluster-name.\"+$cluster_fact && echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | jq  --arg cluster $conman_cluster_name  .'keys[$cluster]'` | sudo tee --append /etc/default/cluster-name", 
         path => [ "/bin/", "/usr/bin" ],
     }
 }
