da91c09c781ce7c01db5b5caaa0c6c4cb9b5169e
udit jain <udit.jain@flipkart.com>
2016-01-13 17:09:18 +0530

    sdfkskd

--- environments/staging/manifests/classes/sample.pp
+++ environments/staging/manifests/classes/sample.pp
@@ -3,7 +3,6 @@ class sample {
     $bucket = $::configbucket
     $cluster_fact = $::uniquefact
     $conman_cluster_name = sprintf('conman-cluster-name%s','$cluster_fact')
-    sprintf('hello user %s', 'CoolUser')
     exec { "update-cluster-name":
         path => [ "/bin/", "/usr/bin" ],
         command => "echo `curl -s \"http://10.47.0.101/v1/buckets/$bucket\" | grep -o '\"$conman_cluster_name\":[^,]*' | cut -d '\"' -f4` | sudo tee --append /etc/default/cluster-name", 
