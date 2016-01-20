44996b373c739ed16509c55801b2dc76e5591e2f
udit jain <udit.jain@flipkart.com>
2016-01-20 18:22:34 +0530

    resolve error

--- environments/sherlocknewinstance/manifests/classes/application.pp
+++ environments/sherlocknewinstance/manifests/classes/application.pp
@@ -1,6 +1,6 @@
 class application {
   
-    exec { "fk-w3-sherlock":
+    exec { "fk-w3-sherlock-old":
         command => "sudo apt-get -y --allow-unauthenticated --force-yes install fk-w3-sherlock",
         path => "/usr/bin",
         logoutput => true,
