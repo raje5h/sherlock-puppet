53661c56bd039aee41ce073f19d361f9bd899cac
udit jain <udit.jain@flipkart.com>
2016-01-21 12:43:19 +0530

    node definition in single file

--- environments/sherlocknewinstance/manifests/nodes/sherlock-app-cloud-sherlock-aggregator.pp
+++ environments/sherlocknewinstance/manifests/nodes/sherlock-app-cloud-sherlock-aggregator.pp
@@ -8,3 +8,13 @@ node /^sherlock-app-cloud-sherlock-aggregator/ {
         
         Class['commonsetup'] -> Class['applicationsources'] -> Class['haproxy'] -> Class['applicationsetup'] -> Class['cosmos'] -> Class['alertz']
 }
+
+node /^sherlock-app-cloud/ {
+        include commonsetup
+        include applicationsources
+        include applicationsetup
+        include cosmos
+        include alertz
+        
+        Class['commonsetup'] -> Class['applicationsources'] -> Class['applicationsetup'] -> Class['cosmos'] -> Class['alertz']
+}
