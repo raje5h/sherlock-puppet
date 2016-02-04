babbe40f661a80ed4aa23e4a551e23e4d15fe86e
udit jain <udit.jain@flipkart.com>
2016-02-04 12:56:42 +0530

    ads

--- environments/restart/manifests/classes/sherlockrestart.pp
+++ environments/restart/manifests/classes/sherlockrestart.pp
@@ -5,6 +5,7 @@ class sherlockrestart {
     exec { "restart":
             command => "sudo /etc/init.d/fk-w3-sherlock restart",
             logoutput => true,
+            timeout => 1800,
             path => "/usr/bin/",
         }
 
