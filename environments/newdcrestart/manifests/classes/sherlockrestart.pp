0dbbf444818ae8e1626b57fb3a32c548962ecd65
udit jain <udit.jain@flipkart.com>
2016-02-26 19:40:28 +0530

    dfsdf

--- environments/newdcrestart/manifests/classes/sherlockrestart.pp
+++ environments/newdcrestart/manifests/classes/sherlockrestart.pp
@@ -3,6 +3,7 @@
     exec { "restart":
             command => "sudo /etc/init.d/fk-w3-sherlock restart",
             logoutput => true,
+            timeout => 1800,
             path => "/usr/bin/",
         }
 
