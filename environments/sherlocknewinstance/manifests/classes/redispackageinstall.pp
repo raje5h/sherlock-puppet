0693fead30ab4db24c0830187303c9410a9c7921
udit jain <udit.jain@flipkart.com>
2016-03-15 00:04:30 +0530

    export fix 112

--- environments/sherlocknewinstance/manifests/classes/redispackageinstall.pp
+++ environments/sherlocknewinstance/manifests/classes/redispackageinstall.pp
@@ -34,7 +34,7 @@ class redispackageinstall {
     }
     
     exec { "export-redis-bucket-2":
-        command => 'true && source /etc/default/redis',
+        command => 'true && ./etc/default/redis',
         provider => shell,
         path => [ "/bin/", "/usr/bin", "/sbin" ],
         require => Exec["export-redis-bucket"],
