734417c9075f2f64a3857655990789937f32a984
udit jain <udit.jain@flipkart.com>
2016-04-28 14:58:34 +0530

    perf deploy

--- environments/newdcdeploy/manifests/nodes/sherlock-app-cloud.pp
+++ environments/newdcdeploy/manifests/nodes/sherlock-app-cloud.pp
@@ -2,6 +2,6 @@ node /^sherlock-(internal-app|app)-cloud/ {
         include sherlockclouddeploy
 }
 
-node /^sherlock-test-env-preprod/ {
+node /^sherlock-test-env-(preprod|perf)/ {
         include sherlockclouddeploy
 }
