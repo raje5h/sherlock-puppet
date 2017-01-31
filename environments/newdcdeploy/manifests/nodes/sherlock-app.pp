5091feb69190dc2f5f9f9e1c791f6d1c51d84836
Sunil Kumar <sunilkumar.d@flipkart.com>
2017-01-31 21:01:11 +0530

    [DS-768] puppet support for base index deployment.

--- environments/newdcdeploy/manifests/nodes/sherlock-app.pp
+++ environments/newdcdeploy/manifests/nodes/sherlock-app.pp
@@ -1,3 +1,21 @@
+
+
+node /^sherlock-app-base.*aggregator/ {
+        include sherlockclouddeploy
+        include tcpsettings
+        include etchostrefresh
+
+        Class['etchostrefresh'] -> Class['tcpsettings'] -> Class['sherlockclouddeploy']
+}
+
+node /^sherlock-app-base.*solr-only/ {
+        include sherlocksolrdeploy
+        include tcpsettings
+        include etchostrefresh
+
+        Class['etchostrefresh'] -> Class['tcpsettings'] -> Class['sherlocksolrdeploy']
+}
+
 node /^sherlock-app/ {
         include sherlockdeploy
 }
