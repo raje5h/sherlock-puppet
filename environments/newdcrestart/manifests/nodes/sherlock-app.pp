bbed8b78b32a53a4f68afd5ffeed0ee9b7084a3b
Sunil Kumar <sunilkumar.d@flipkart.com>
2017-02-01 17:33:27 +0530

    [DS-1021] restart support for base index cluster.

--- environments/newdcrestart/manifests/nodes/sherlock-app.pp
+++ environments/newdcrestart/manifests/nodes/sherlock-app.pp
@@ -7,6 +7,14 @@ node /^sherlock-(internal-app|app)-cloud/ {
         include shardrestart
 }
 
+node /^sherlock-app-base.*aggregator/ {
+        include sherlockrestart
+}
+
+node /^sherlock-app-base.*solr-only/ {
+        include shardrestart
+}
+
 node /^sherlock-test-env.*aggregator/ {
         include sherlockrestart
 }
